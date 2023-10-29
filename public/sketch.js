1c863a347d2b00d5302b217df405824f5155e64e
AndresNei20 <andresfelipeneira2004@gmail.com>
2023-10-29 15:27:56 -0500

    timer & game over overlay

--- public/sketch.js
+++ public/sketch.js
@@ -6,6 +6,14 @@ let shinySpawnInterval = 5000;
 let lastSpawnTime = 0;
 let lastShinySpawnTime = 0;
 
+//timer
+let count = 5;
+let countInterval = 1000;
+let lastCountUpdate = 0;
+
+//game status
+let isGameOver = false
+
 class Zombie{
   constructor(img, speed){
     this.x = 0;
@@ -30,21 +38,39 @@ function preload() {
 }
 
 function setup() {
+  frameRate(60)
   createCanvas(400, 400);
 }
 
 function draw() {
   background(220);
 
-  if(millis() - lastSpawnTime > spawnInterval){
+  //timer
+  if (!isGameOver && millis() - lastCountUpdate > countInterval && count > 0) {
+    count--;
+    lastCountUpdate = millis();
+  }
+  textSize(24);
+  fill(0);
+  text(`Tiempo restante: ${count} segundos`, 10, 30);
+
+  if(count == 0){
+    isGameOver = true
+    //esto hara que cuando se active el return PARA por completo el draw y todo lo que vaya despues
+  }
+
+
+    //normal_zombie spaws
+    if(!isGameOver && millis() - lastSpawnTime > spawnInterval){
       let newZombie = new Zombie(normal_zombie, 3);
       let newZombie2 = new Zombie(normal_zombie, 3);
       zombies.push(newZombie);
       zombies.push(newZombie2);
-    lastSpawnTime = millis();
-  }
-
-  if(millis() - lastShinySpawnTime > shinySpawnInterval){
+      lastSpawnTime = millis();
+    }
+    
+    //shiny_zombie spaws
+  if(!isGameOver && millis() - lastShinySpawnTime > shinySpawnInterval){
     let newShinyZombie = new Zombie(shiny_zombie, 6);
     zombies.push(newShinyZombie);
     lastShinySpawnTime = millis();
@@ -57,5 +83,17 @@ function draw() {
     if (zombies[i].x < -50) {
       zombies.splice(i, 1); 
     }
+  }
+
+  //GameOver overlay
+  if(isGameOver){
+    fill(0, 100);
+    rect(0, 0, width, height);
+
+    textSize(32);
+    fill(255);
+    textAlign(CENTER, CENTER);
+    text('Game Over', width / 2, height / 2 -20);
+    text(`Score: ${spawnInterval}`, width / 2, height / 2+20)
   }
 }
